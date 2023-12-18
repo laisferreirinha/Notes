cb5454053012ddc09b492844e327c0fa9c78842b
Laís Ferreira <laissferreira@icloud.com>
2023-12-18 00:49:08 +0000

    Added the menu to the actionbars

--- App/app/src/main/java/pt/iade/laisferreira/atividade/NoteActivity.java
+++ App/app/src/main/java/pt/iade/laisferreira/atividade/NoteActivity.java
@@ -1,8 +1,12 @@
 package pt.iade.laisferreira.atividade;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -10,5 +14,32 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        setupComponents();
     }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_save_item) {
+            //Actionbar "Add" button.
+            finish();
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupComponents () {
+        //Setup the Actionbar.
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+    }
+
+
 }
